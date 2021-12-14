ac4ff401ea869eb5f0b93ad2fc1afb55bddf15c8
Benjamin Maggi <bmaggi@salesforce.com>
2021-12-14 10:01:54 -0300

    test: istanbul ignore worker.js never paths

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -2,8 +2,9 @@ const path = require('path');
 const { workerData } = require('worker_threads');
 
 require('ts-node').register();
-if (process.env.NODE_ENV === 'test') {
+/* istanbul ignore next */
+if (process.env.NODE_ENV === 'test' && process.env.NODE_WORKER) {
   require(process.env.NODE_WORKER);
-} else {
-  require(path.resolve(__dirname, workerData.path));
 }
+/* istanbul ignore next */
+require(path.resolve(__dirname, workerData.path));
