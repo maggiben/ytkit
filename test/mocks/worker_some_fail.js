470672963b7292406cb52c539bf12756053ab9de
Benjamin Maggi <bmaggi@salesforce.com>
2021-12-14 09:45:28 -0300

    fix: remove optional chaining in js file for node12 support

--- test/mocks/worker_some_fail.js
+++ test/mocks/worker_some_fail.js
@@ -2,6 +2,6 @@ const { parentPort, isMainThread } = require('worker_threads');
 
 if (!isMainThread) {
   const shouldError = Math.floor(Math.random() * 2);
-  shouldError && parentPort?.postMessage('error', new Error('Worker error'));
+  shouldError && parentPort && parentPort.postMessage('error', new Error('Worker error'));
   process.exit(shouldError);
 }
